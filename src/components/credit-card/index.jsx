919d71eeb6c01d27d0fde0b9ed1b43a9943fe3aa
Giovanni Souza <giovanni.souza@novatendencia.localdomain>
2019-12-16 18:01:42 -0300

    Add images

--- src/components/credit-card/index.jsx
+++ src/components/credit-card/index.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
 import './index.model.css';
 
-export default props => <div className='credit-card shadow rounded'></div>;
+import Logo from '../../assets/images/png/efigie.png';
+import Chip from '../../assets/images/png/card-chip.png';
+
+export default props => (
+  <div className='credit-card shadow rounded p-3'>
+    <img
+      src={Logo}
+      className='mx-auto d-block'
+      alt='generic credit card logo'
+    />
+    <img src={Chip} className='mt-3' alt='generic credit card chip' />
+    <div className='row d-flex justify-content-center'>
+      <label>0000 0000 0000 0000</label>
+    </div>
+  </div>
+);
