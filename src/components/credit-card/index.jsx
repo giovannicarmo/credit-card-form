1bdc3eaa5b348fd693e514a48c952e17d0b23b69
Giovanni Souza <giovanni.souza@novatendencia.localdomain>
2019-12-18 17:57:35 -0300

    Install MaskInput dependencie

--- src/components/credit-card/index.jsx
+++ src/components/credit-card/index.jsx
@@ -3,6 +3,7 @@ import './index.model.css';
 
 import Logo from '../../assets/images/png/efigie.png';
 import Chip from '../../assets/images/png/card-chip.png';
+import MasterCard from '../../assets/images/png/logo-mastercard.png';
 
 export default props => (
   <div className='credit-card shadow rounded p-3'>
@@ -11,9 +12,34 @@ export default props => (
       className='mx-auto d-block'
       alt='generic credit card logo'
     />
-    <img src={Chip} className='mt-3' alt='generic credit card chip' />
-    <div className='row d-flex justify-content-center'>
-      <label>0000 0000 0000 0000</label>
+    <img src={Chip} className='mt-1' alt='generic credit card chip' />
+    <div className='row d-flex justify-content-between'>
+      <div className='col-md-8 mt-3'>
+        <span>{props.number || "0000 0000 0000 0000"}</span>
+      </div>
+      <div className='col-md-4'>
+        <div className='d-flex align-items-start flex-column'>
+          <div>
+            <label>Exp. date:</label>
+          </div>
+          <div className='mt-075'>
+            <span className='card-date'>{props.date || "00/0000"}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='row d-flex justify-content-between'>
+      <div className='col-md-6'>
+        <div>
+          <label>Card name:</label>
+        </div>
+        <div className='mt-075'>
+<span className='text-uppercase'>{props.name || "your name here"}</span>
+        </div>
+      </div>
+      <div className='col-md-6 text-right'>
+        <img src={MasterCard} alt='card flag logo' />
+      </div>
     </div>
   </div>
 );
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,57 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CreditCard from '../credit-card';
+import MaskInput from 'react-maskinput';
 import './index.model.css';
 
-export default () => (
-  <div className='row d-flex justify-content-center'>
-    <div className='card'>
-      <div className='card-img-top d-flex justify-content-center'>
-        <CreditCard></CreditCard>
-      </div>
-      <div className='cardBody p-3 pt-5 shadow rounded'>
-        <form>
-          <div className='form-row'>
-            <div className='col-md-12'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Card Number'
-              />
+export default () => {
+  const [cardNumberMask, setCardNumberMask] = useState('0000-0000-0000-0000');
+  const [expDateMask, setExpDateMask] = useState('mm/yy');
+
+  const handleChange = e => {
+    if (
+      e.target.value.indexOf('34') === 0 ||
+      e.target.value.indexOf('37') === 0
+    ) {
+      setCardNumberMask('0000-000000-00000');
+      return;
+    }
+
+    setCardNumberMask('0000-0000-0000-0000');
+  };
+
+  return (
+    <div className='row d-flex justify-content-center'>
+      <div className='card'>
+        <div className='card-img-top d-flex justify-content-center'>
+          <CreditCard number={cardNumberMask}></CreditCard>
+        </div>
+        <div className='cardBody p-3 pt-5 shadow rounded'>
+          <form>
+            <div className='form-row'>
+              <div className='col-md-12'>
+                <MaskInput
+                  onChange={handleChange}
+                  maskChar='_'
+                  mask={cardNumberMask}
+                  alwaysShowMask
+                  className='form-control'
+                />
+              </div>
             </div>
-          </div>
-          <div className='form-row mt-3'>
-            <div className='col-md-12'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Card Name'
-              />
+            <div className='form-row mt-3'>
+              <div className='col-md-12'>
+                <input
+                  type='text'
+                  className='form-control'
+                  placeholder='Card Name'
+                />
+              </div>
             </div>
-          </div>
-          <div className='form-row mt-3 d-flex justify-content-between'>
-            <div className='col-md-5'>
-              <input
-                type='text'
-                className='form-control'
-                placeholder='Exp. Date'
-              />
+            <div className='form-row mt-3 d-flex justify-content-between'>
+              <div className='col-md-5'>
+                <MaskInput
+                  alwaysShowMask
+                  maskChar='_'
+                  mask='00/00'
+                  className='form-control'
+                />
+              </div>
+              <div className='col-md-5'>
+                <input type='text' className='form-control' placeholder='CVV' />
+              </div>
             </div>
-            <div className='col-md-5'>
-              <input type='text' className='form-control' placeholder='CVV' />
+            <div className='form-row mt-3'>
+              <div className='col-md-12'>
+                <button
+                  type='submit'
+                  className='btn btn-success btn-lg btn-block'
+                  disabled
+                >
+                  Submit
+                </button>
+              </div>
             </div>
-          </div>
-          <div className='form-row mt-3'>
-            <div className='col-md-12'>
-              <button
-                type='submit'
-                className='btn btn-success btn-lg btn-block'
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
